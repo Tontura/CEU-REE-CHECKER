f7ed7bb5d83c5db764e45d5096f39b0ea4ae7bd0
Tontura <tontscortez@hotmail.com>
2026-07-02 09:11:01 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["pdfjs-dist"],
 };
 
 export default nextConfig;
